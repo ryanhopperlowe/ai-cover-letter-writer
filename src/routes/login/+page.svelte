5321a960bba02546aeb26721b3b688f65d0ccb81
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2025-02-04 22:40:33 -0600

    feat: build out login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import type { ActionData } from './$types';
 
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login/Register</h1>
-<form method="post" action="?/login" use:enhance>
-	<label>
-		Username
-		<input name="username" />
-	</label>
-	<label>
-		Password
-		<input type="password" name="password" />
-	</label>
-	<button>Login</button>
-	<button formaction="?/register">Register</button>
-</form>
-<p style="color: red;">{form?.message ?? ''}</p>
+<div class="flex h-screen justify-center pt-20">
+	<div class="flex w-full max-w-2xl flex-col gap-4">
+		<h1 class="text-2xl font-bold">Login/Register</h1>
+
+		<form method="post" action="?/login" use:enhance class="flex flex-col gap-4">
+			<label>
+				Username
+				<Input name="username" />
+			</label>
+
+			<label>
+				Password
+				<Input type="password" name="password" />
+			</label>
+
+			<nav class="flex flex-row-reverse gap-4">
+				<Button type="submit">Login</Button>
+				<Button variant="outline" type="submit" formaction="?/register">Register</Button>
+			</nav>
+		</form>
+		<p style="color: red;">{form?.message ?? ''}</p>
+	</div>
+</div>
