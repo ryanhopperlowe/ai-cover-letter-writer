3a06bb9a5fa1f874d436dcc2a184c2e2c71d7a8c
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2025-02-09 23:38:58 -0600

    add new dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { kitRoutes } from 'vite-plugin-kit-routes';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), kitRoutes()],
+
+	server: {
+		fs: {
+			allow: ['static']
+		}
+	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
