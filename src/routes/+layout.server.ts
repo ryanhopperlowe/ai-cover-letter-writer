065a0b1dbbde02d529ee809667723f7e9f177277
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2025-02-23 21:44:30 -0600

    add coming soon banner

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,8 @@
+import { NODE_ENV } from '$env/static/private';
+import { route } from '$lib/ROUTES';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-	return { user: locals.user };
+export const load: LayoutServerLoad = async ({ url }) => {
+	if (NODE_ENV === 'production' && url.pathname !== route('/')) redirect(302, route('/'));
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Build My Cover Letter</h1>
+<script lang="ts">
+	import { blur } from 'svelte/transition';
+
+	let text = 'Coming Soon';
+
+	let mounted = $state(false);
+	$effect(() => {
+		const timeout = setTimeout(() => {
+			mounted = true;
+			console.log('mounted');
+		}, 1000);
+
+		return () => clearTimeout(timeout);
+	});
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+	<h1>
+		{#each Array.from(text) as char, i}
+			{#if mounted}
+				<span in:blur={{ delay: i * 100, amount: 10, duration: 1000 }}>{char}</span>
+			{/if}
+		{/each}
+	</h1>
+</div>
